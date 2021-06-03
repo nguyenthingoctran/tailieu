bd05920accd64957b8cbc04ae7c11f42a4601231
nguyenthingoctran <tran@leadplus.net>
2021-06-03 17:31:09 +0700

    3/6/2021

---
+++ Front-ent/React/udemy.com/project/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+import Card from '../UI/Card'
+
+const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title);
+
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = 'Updated!';
+  }
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">
+          ${props.amount}
+        </div>
+      </div>
+      <button type="" onClick={clickHandler}>Change Title</button>
+    </Card>
+  ) 
+}
+
+export default ExpenseItem;
