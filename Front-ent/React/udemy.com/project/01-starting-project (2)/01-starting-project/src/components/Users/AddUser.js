e2ab099a6786ebf439e828434c60e3c2f429b790
nguyenthingoctran <tran@leadplus.net>
2021-06-14 17:25:26 +0700

    14/6/2021

--- Front-ent/React/udemy.com/project/01-starting-project (2)/01-starting-project/src/components/Users/AddUser.js
+++ Front-ent/React/udemy.com/project/01-starting-project (2)/01-starting-project/src/components/Users/AddUser.js
@@ -10,38 +10,29 @@ const AddUser = props => {
     const nameInputRef = useRef();
     const ageInputRef = useRef();
 
-    const [enteredUsername, setEnterUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
     const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
         const enteredName = nameInputRef.current.value;
-        const enteredAge = ageInputRef.current.value;
-        if (enteredUsername.trim().length === 0 || enteredAge.trim().length ===0){
+        const enteredUserAge = ageInputRef.current.value;
+        if (enteredName.trim().length === 0 || enteredUserAge.trim().length ===0){
             setError({
                 title: 'Invalid input',
                 message: 'Please enter a valid name and age (non-empty values)'
             });
         }
-        if (+enteredAge < 1){
+        if (+enteredUserAge < 1){
             setError({
                 title: 'Invalid age',
                 message: 'Please enter a valid age (> 0)'
             });
+            return;
         }
-        props.onAddUser(enteredUsername, enteredAge)
-        setEnterUsername('');
-        setEnteredAge('');
+        props.onAddUser(enteredName, enteredUserAge)
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     }
-
-    const usenameChangeHandler = (event) => {
-        setEnterUsername(event.target.value);
-    };
-
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
-    };
 
     const errorHandler = () => {
         setError(null);
@@ -58,15 +49,11 @@ const AddUser = props => {
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
                     <input id="username" 
-                            type="text" 
-                            value={enteredUsername}
-                            onChange={usenameChangeHandler} 
+                            type="text"  
                             ref={nameInputRef}/>
                     <label htmlFor="age">Age (Years)</label>
                     <input id="age" 
                             type="number" 
-                            value={enteredAge}
-                            onChange={ageChangeHandler}
                             ref={ageInputRef}/>
                     <Button type="submit">Add User</Button>
                 </form>    
