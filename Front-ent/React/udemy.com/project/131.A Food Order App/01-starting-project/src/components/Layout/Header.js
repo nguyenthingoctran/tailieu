5dff16b8d0c769b34e82b3f2c8dfbcae47acedc8
DESKTOP-EPRIB9O\ADMIN <tran@leadplus.net>
2021-06-20 21:43:18 +0700

    20/6/2021

--- Front-ent/React/udemy.com/project/131.A Food Order App/01-starting-project/src/components/Layout/Header.js
+++ Front-ent/React/udemy.com/project/131.A Food Order App/01-starting-project/src/components/Layout/Header.js
@@ -1,14 +1,17 @@
 import { Fragment } from "react";
 
+import HeaderCartButton from "./HeaderCartButton";
+
 import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
 const Header = props => {
   return <Fragment>
-    <header>
+    <header className={classes.header}>
       <h1>ReactMeals</h1>
-      <button>Cart</button>
+      <HeaderCartButton/>
     </header>
-    <div>
+    <div className={classes['main-image']}>
       <img src={mealsImage} alt="A table full of delicious food!"/>
     </div>
   </Fragment>
---
+++ Front-ent/React/udemy.com/project/131.A Food Order App/01-starting-project/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,13 @@
+import Input from '../../UI/Input'
+import classes from './MealItemForm.module.css';
+
+const MealItemForm = (props) => {
+    return (
+        <form className={classes.form}>
+            <input />           
+            <button type="">+ Add</button> 
+        </form>
+    )
+}
+
+export default MealItemForm;
