e9ea3ef1f6ecf1e355dbea56a2a862520bdff826
nguyenthingoctran <tran@leadplus.net>
2021-06-21 11:57:17 +0700

    21/6/2021

--- Front-ent/React/udemy.com/project/131.A Food Order App/01-starting-project/src/components/Layout/HeaderCartButton.js
+++ Front-ent/React/udemy.com/project/131.A Food Order App/01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -2,7 +2,7 @@ import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCardButton.module.css'
 
 const HeaderCartButton = props => {
-    return <button className={classes.button}>
+    return <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}><CartIcon/></span>
         <span>Your Card</span>
         <span className={classes.badge}>3</span>
--- Front-ent/React/udemy.com/project/131.A Food Order App/01-starting-project/src/components/Meals/MealItem/MealItemForm.js
+++ Front-ent/React/udemy.com/project/131.A Food Order App/01-starting-project/src/components/Meals/MealItem/MealItemForm.js
@@ -4,7 +4,14 @@ import classes from './MealItemForm.module.css';
 const MealItemForm = (props) => {
     return (
         <form className={classes.form}>
-            <input />           
+            <Input label="Amount" input={{
+                id: 'amount_' + props.id,
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+            }} />     
             <button type="">+ Add</button> 
         </form>
     )
