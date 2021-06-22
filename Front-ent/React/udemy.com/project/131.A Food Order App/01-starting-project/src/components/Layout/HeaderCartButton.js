64b0c42cf324fbbf8aefca7cb4bdb1d361c9ac61
nguyenthingoctran <tran@leadplus.net>
2021-06-22 17:20:48 +0700

    22/6/2021

--- Front-ent/React/udemy.com/project/131.A Food Order App/01-starting-project/src/components/Layout/HeaderCartButton.js
+++ Front-ent/React/udemy.com/project/131.A Food Order App/01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,19 @@
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderCardButton.module.css'
+import classes from './HeaderCardButton.module.css';
+import CartContext from '../../store/cart-context';
+import { useContext } from 'react';
 
 const HeaderCartButton = props => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartitems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
     return <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}><CartIcon/></span>
         <span>Your Card</span>
-        <span className={classes.badge}>3</span>
+        <span className={classes.badge}>{numberOfCartitems}</span>
     </button>
 }
 
